b88d797626466ff32df2770d0d89da0ef8bc3921
MironFoerster <miron.foerster@gmx.de>
2021-12-21 19:17:04 +0100

    add cache for cmd_list queueing

--- SCRIBUSwebsite/draw/apps.py
+++ SCRIBUSwebsite/draw/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class DrawConfig(AppConfig):
     name = 'draw'
+    def ready(self):
+        # code for startup, initialize cache variables
+        cache.set('cmd_list_queue', [])
